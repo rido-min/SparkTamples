a8d69f5b45cc96cd1878c5aedf620d46d8a4c332
Ricardo Minguez Pablos (RIDO) <rmpablos@microsoft.com>
2025-09-19 13:03:44 -0700

    start

---
+++ Spark.Echo/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.Teams.Plugins.AspNetCore.Extensions;
+using Microsoft.Teams.Apps.Activities;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.AddTeams();
+var webApp = builder.Build();
+
+var teamsApp = webApp.UseTeams();
+
+teamsApp.OnMessage(async context =>
+{
+    await context.Send("Echo: " + context.Activity.Text);
+});
+
+webApp.Run();
---
+++ Spark.McpClient.MinApi/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.Teams.AI.Models.OpenAI;
+using Microsoft.Teams.AI.Prompts;
+using Microsoft.Teams.Apps;
+using Microsoft.Teams.Apps.Activities;
+using Microsoft.Teams.Plugins.AspNetCore.Extensions;
+using Microsoft.Teams.Plugins.External.McpClient;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.AddTeams();
+WebApplication webApp = builder.Build();
+
+OpenAIChatPrompt prompt = new(
+           new OpenAIChatModel(
+               model: "gpt-4o",
+               apiKey: Environment.GetEnvironmentVariable("OPENAI_API_KEY")!),
+            new ChatPromptOptions()
+                .WithDescription("helpful assistant")
+                .WithInstructions(
+                    "You are a helpful assistant that can help answer questions using Microsoft docs.",
+                    "You MUST use tool calls to do all your work.")
+                 );
+prompt.Plugin(new McpClientPlugin().UseMcpServer("https://learn.microsoft.com/api/mcp"));
+
+App app = webApp.UseTeams();
+app.OnMessage(async context =>
+{
+    await prompt.Send(context.Activity.Text, new(), (chunk) => Task.Run(() =>
+    {
+        context.Stream.Emit(chunk);
+    }), context.CancellationToken);
+});
+webApp.Run();
+
+
+
+
