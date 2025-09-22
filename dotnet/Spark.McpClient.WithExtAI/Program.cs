dab0d5320985cd1398177f87b3165cf7efa9abe8
Ricardo Minguez Pablos (RIDO) <rmpablos@microsoft.com>
2025-09-22 14:05:17 -0700

    upd MEAI

--- dotnet/Spark.McpClient.WithExtAI/Program.cs
+++ dotnet/Spark.McpClient.WithExtAI/Program.cs
@@ -1,39 +1,47 @@
-using Microsoft.Teams.Plugins.AspNetCore.Extensions;
-using Microsoft.Teams.Apps.Activities;
-using ModelContextProtocol.Client;
 using Microsoft.Extensions.AI;
+using Microsoft.Teams.Api.Activities;
+using Microsoft.Teams.Apps.Activities;
+using Microsoft.Teams.Plugins.AspNetCore.Extensions;
+using ModelContextProtocol.Client;
 using OpenAI;
 using ChatMessage = Microsoft.Extensions.AI.ChatMessage;
+
+IChatClient client =
+    new ChatClientBuilder(new OpenAIClient(Environment.GetEnvironmentVariable("OPENAI_API_KEY")!).GetChatClient("gpt-4o").AsIChatClient())
+    .UseFunctionInvocation()
+    .UseLogging(LoggerFactory.Create(b => b.AddConsole().SetMinimumLevel(LogLevel.Information)))
+    .Build();
+
+var mcpClient = await McpClientFactory.CreateAsync(
+    new SseClientTransport(new () { 
+        Endpoint = new Uri("https://learn.microsoft.com/api/mcp"), 
+        TransportMode = HttpTransportMode.AutoDetect, 
+        Name = "msdocs" }));
+
+var tools = await mcpClient.ListToolsAsync();
+Console.WriteLine("Tools Found: " + string.Join(", ", tools.Select(t => t.Name)));
+
+var chatOptions = new ChatOptions
+{
+    AllowMultipleToolCalls = true,
+    Instructions = "Use the following tools to answer the user's question. If you don't know the answer, use the 'Search Microsoft Docs' tool to find relevant information.",
+    Tools = [.. tools]
+};
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddTeams();
 var webApp = builder.Build();
 
-IChatClient client =
-    new ChatClientBuilder(new OpenAIClient(Environment.GetEnvironmentVariable("OPENAI_API_KEY")!).GetChatClient("gpt-4o").AsIChatClient())
-    .UseFunctionInvocation()
-    .Build();
-
-var mcpClient = await McpClientFactory.CreateAsync(new SseClientTransport(new SseClientTransportOptions() { Endpoint = new Uri("https://learn.microsoft.com/api/mcp") }));
-var tools = await mcpClient.ListToolsAsync();
-
-var chatOptions = new ChatOptions
-{
-    Tools = [.. tools]
-};
-
-List<ChatMessage> chatHistory = [new(ChatRole.System, """
-    You are a helpful assistant that can help answer questions using Microsoft docs.
-    You MUST use tool calls to do all your work.
-    """)];
-
 var teamsApp = webApp.UseTeams();
 
 teamsApp.OnMessage(async context =>
 {
-    chatHistory.Add(new ChatMessage(ChatRole.User, context.Activity.Text));
-    ChatResponse response = await client.GetResponseAsync(chatHistory, chatOptions);
+    await context.Send(new TypingActivity());
+    ChatResponse response = await client.GetResponseAsync([new ChatMessage(ChatRole.User, context.Activity.Text)], chatOptions);
     await context.Send(response.Text);
+    var cc = response.RawRepresentation as OpenAI.Chat.ChatCompletion;
 });
 
 webApp.Run();
