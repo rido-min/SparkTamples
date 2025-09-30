9a74f9f393da26a446a0a4b6d0e9cd85895650f1
Ricardo Minguez Pablos (RIDO) <rmpablos@microsoft.com>
2025-09-30 08:36:46 -0700

    add BF Echo

---
+++ dotnet/BF.Echo/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Bot.Schema;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<BotFrameworkAuthentication, ConfigurationBotFrameworkAuthentication>();
+
+builder.Services.AddSingleton<IBotFrameworkHttpAdapter>(provider => 
+    new CloudAdapter(
+        provider.GetRequiredService<BotFrameworkAuthentication>(),
+        provider.GetRequiredService<ILogger<CloudAdapter>>()));
+builder.Services.AddTransient<IBot, EchoBot>();
+
+var app = builder.Build();
+
+app.MapPost("/api/messages", async (IBotFrameworkHttpAdapter adapter, IBot bot, HttpRequest request, HttpResponse response) => 
+    await adapter.ProcessAsync(request, response, bot));
+
+app.Run();
+
+class EchoBot: ActivityHandler
+{
+    protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+    {
+        var replyText = $"Echo: {turnContext.Activity.Text}";
+        await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+    }
+}
